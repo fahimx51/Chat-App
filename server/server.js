6f5116c19d4d7802447c48ee3ead3865a518b1d6
fahimx51 <fahim69.dev@gmail.com>
2026-03-25 14:25:02 +0600

    added user routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 //create express app
 const app = express();
@@ -13,9 +14,11 @@ const server = http.createServer(app);
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
+//Routes setup
 app.use("/api/status", (req, res) => {
     res.send("<h1>Server is live!</h1>");
 });
+app.use("/api/auth", userRouter);
 
 // connect DB
 await connectDB();
